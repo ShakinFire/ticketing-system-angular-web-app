f18d872686c5709b0b93157d9e8fdee88bcb5f3f
WeatherBug <georgienevv@gmail.com>
2018-04-18 02:48:57 +0300

    Working on the homepage. Only the footer is left

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,21 @@
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './nav/nav.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    NgbModule,
   ],
-  declarations: []
+  declarations: [
+    NavComponent,
+    FooterComponent,
+  ],
+  exports: [
+    NavComponent,
+    FooterComponent,
+  ]
 })
 export class SharedModule { }

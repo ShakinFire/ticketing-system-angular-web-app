d94cfd0404a803ca878a3eea52d256700c981d2b
DESKTOP-E0R3LIQ\Tsvetelina <georgienevv@gmail.com>
2018-04-18 12:15:22 +0300

    finished the footer and the whole homepage

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,13 @@ import { NavComponent } from './nav/nav.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   imports: [
     CommonModule,
     NgbModule,
+    AngularFontAwesomeModule,
   ],
   declarations: [
     NavComponent,

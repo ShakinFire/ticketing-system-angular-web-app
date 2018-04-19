3b7a28eda6fc266d9d4a584dc12297d754530d30
WeatherBug <georgienevv@gmail.com>
2018-04-19 23:46:29 +0300

    Added fron-end validations on the forms

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms/src/model';
+import { FormControl, FormGroup } from '@angular/forms/src/model';
+import { NgForm } from '@angular/forms/src/directives/ng_form';
 
 @Component({
   selector: 'app-auth',
@@ -8,22 +9,56 @@ import { FormControl } from '@angular/forms/src/model';
 })
 export class AuthComponent implements OnInit {
   formSwitch: boolean;
-  isActive: boolean;
+  loginData: Object;
+  errorMessage: string;
+  isError: boolean;
+
   constructor() { }
 
   ngOnInit() {
     this.formSwitch = false;
-    this.isActive = false;
+    this.isError = false;
   }
 
   formShow(): boolean {
+    if (this.isError) {
+      this.isError = false;
+    }
+
     if(this.formSwitch) {
-      this.isActive = false;
       this.formSwitch = false;
     } else {
-      this.isActive = true;
       this.formSwitch = true;
     }
     return false;
+  }
+
+  validate(data): string {
+    if (data.password.length < 6) {
+      return 'Error: Password should be at least 6 characters';
+    }
+
+    if (data.username.length < 4) {
+      return 'Error: Username should be at least 4 characters';
+    }
+    if(data.name) {
+      if (data.name.length < 2) {
+        return 'Error: Invalid name length';
+      }
+
+      if (data.password !== data.repeatPassword) {
+        return 'Error: Password does not match';
+      }
+    }
+  }
+
+  formsData(form: FormGroup): void {
+    this.loginData = form.value;
+    this.errorMessage = this.validate(this.loginData);
+    if (this.errorMessage) {
+      this.isError = true;
+    } else {
+      this.isError = false;
+    }
   }
 }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -7,7 +8,8 @@ import { AuthComponent } from './auth.component';
 @NgModule({
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    FormsModule,
   ],
   declarations: [AuthComponent]
 })

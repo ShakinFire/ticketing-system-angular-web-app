ab7c423edeec645dada5db0bbf0ca396e28af7ff
WeatherBug <georgienevv@gmail.com>
2018-04-23 01:58:45 +0300

    connected the authentication and added interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthInterceptor } from './core/auth/auth.interceptor';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -9,11 +10,18 @@ import { SharedModule } from './shared/shared.module';
 import { HomeComponent } from './home/home.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { AuthModule } from './auth/auth.module';
+import { CoreModule } from './core/core.module';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+import { AppConfig } from './config/app-config';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    FooterComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,8 +31,17 @@ import { AuthModule } from './auth/auth.module';
     AppRoutingModule,
     AngularFontAwesomeModule,
     AuthModule,
+    CoreModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    AppConfig,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthService } from './auth/auth.service';
 
 @NgModule({
   imports: [
     CommonModule,
   ],
-  declarations: [
+  declarations: [],
+  providers: [
+    AuthService,
   ],
-  exports: [
-  ]
 })
-export class SharedModule { }
+export class CoreModule { }
